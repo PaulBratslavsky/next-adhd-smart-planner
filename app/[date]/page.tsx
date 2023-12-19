46db92d704f88a2384ae8e7c100943fa2f3b3c49
Paul Bratslavsky <paulbratslavsky@Pauls-MacBook-Pro.local>
2023-12-19 16:46:01 -0600

    fetch day entry from neon db using prisma

--- app/[date]/page.tsx
+++ app/[date]/page.tsx
@@ -1,9 +1,10 @@
 import { getCalendarDayData } from "@/database/models/calendar-days";
+import { Card } from "@/components/ui/card";
 
 interface ParamsProps {
   readonly params: {
     date: string;
-  }
+  };
 }
 
 async function loadDateData(date: string) {
@@ -12,11 +13,22 @@ async function loadDateData(date: string) {
 
 export default async function DynamicDateRoute({ params }: ParamsProps) {
   const data = await loadDateData(params.date);
+
+  if (!data) return <h2>Add Initialize Day Form Here</h2>
+
   return (
-    <div>
-      <h1>Dynamic Date Route</h1>
-      <p>params: {params.date}</p>
-      <p>data: {JSON.stringify(data)}</p>
-    </div>
+    <Card className="p-4 w-full min-h-[calc(100vh-2rem)]">
+      <div className="grid gap-2 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {data?.Task &&
+          data.Task.map((task) => {
+            return (
+              <Card className="p-2" key={task.id}>
+                <h3>{task.title}</h3>
+                <p>{task.description}</p>
+              </Card>
+            );
+          })}
+      </div>
+    </Card>
   );
 }
